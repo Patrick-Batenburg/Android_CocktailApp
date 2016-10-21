f8be7aac964962ad175a56211df601d9fbd2aba1
Patrick <zammypatrick@gmail.com>
2016-10-21 11:59:54 +0200

    design naar xml login en sign in

--- Project/CocktailApp/app/src/main/java/com/example/cocktailapp/LoginActivity.java
+++ Project/CocktailApp/app/src/main/java/com/example/cocktailapp/LoginActivity.java
@@ -1,39 +1,13 @@
 package com.example.cocktailapp;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
+import static android.R.attr.value;
 
 public class LoginActivity extends AppCompatActivity
 {
@@ -50,5 +24,17 @@ public class LoginActivity extends AppCompatActivity
         getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
     }
+
+    public void signIn(View view)
+    {
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
+    }
+
+    public void signUp(View view)
+    {
+
+    }
+
 }
 
